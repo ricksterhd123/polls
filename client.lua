49ac221f2596f52c9e5548c8fdc64f1f8a928c5b
ricksterhd123 <ricksterhd123@gmail.com>
2020-03-04 00:57:55 +0000

    Begin adding event handlers

--- client.lua
+++ client.lua
@@ -1,18 +1,62 @@
+local cursorShowing = false
+local pollMain = nil
+local pollRead = nil
+local pollEdit = nil
+local pollVote = nil
+
 -- Retreives data from models, updates view.
 function main(isAdmin, polls)
-    local pollMain = PollMain(isAdmin, polls)
+    pollMain = PollMain(isAdmin, polls)
     pollMain:show()
+    -- Add
+    addEventHandler("onClientGUIClick", pollMain.button[1], 
+    function ()
+        iprint("Add")
+    end, false)
+    -- Edit
+    addEventHandler("onClientGUIClick", pollMain.button[2], 
+    function ()
+        iprint("Edit")
+    end, false)
+
+    -- Archive
+    addEventHandler("onClientGUIClick", pollMain.button[3], 
+    function ()
+        iprint("Archive")
+    end, false)
+
+    -- Delete
+    addEventHandler("onClientGUIClick", pollMain.button[4], 
+    function ()
+        iprint("Delete")
+    end, false)
+
+    -- Read
+    addEventHandler("onClientGUIDoubleClick", pollMain.gridlist[1], 
+    function()
+        iprint("Read")
+    end, false)
+
     --local pollRead = PollRead()
     --local pollVote = PollVote()
     --local pollEdit = PollEdit()
-    iprint(isAdmin)
+    showCursor(true)
+    cursorShowing = true
 end
 addEvent("onStart", true)
 addEventHandler("onStart", resourceRoot, main)
 
--- Get isAdmin from server
-addEventHandler("onClientResourceStart", resourceRoot, 
+-- Get data from server
+addCommandHandler("polls", 
     function() 
         triggerServerEvent("onPreStart", localPlayer) 
+    end
+)
+
+addEventHandler("onClientResourceStop", resourceRoot, 
+    function()
+        if cursorShowing then
+            showCursor(false)
+        end
     end
 )
